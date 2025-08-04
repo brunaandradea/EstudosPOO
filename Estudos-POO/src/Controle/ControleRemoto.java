7a7bd96bb7c7bfa9bb15279e045ba4d33fcde5ca
brunaandradea <179890@unifio.edu.br>
2025-08-04 17:53:56 -0300

    `Refactored ControleRemoto class to implement Controlador interface and added getter and setter methods for attributes.`

---
+++ Estudos-POO/src/Controle/ControleRemoto.java
@@ -0,0 +1,15 @@
+package Controle;
+
+public class ControleRemoto {
+    private int volume;
+    private boolean ligado;
+    private boolean tocando;
+
+    public ControleRemoto() {
+        this.volume = 50;
+        this.ligado = falso;
+        this.tocando = falso;
+    }
+
+    
+}
